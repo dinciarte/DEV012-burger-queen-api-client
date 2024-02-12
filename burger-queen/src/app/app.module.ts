c162849595fdc14647436804f558d1a4c7cb7531
Diveana Inciarte <dinciarte25@gmail.com>
2024-02-12 13:10:10 -0300

    Se crea el nuevo proyecto

---
+++ burger-queen/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/solo/login/login.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
