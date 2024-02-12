344b15e85dcd25f156a91656244d1653f3cec874
Diveana Inciarte <dinciarte25@gmail.com>
2024-02-12 16:18:39 -0300

    Se añade contenido de login antes de iniciar con el servicio

--- burger-queen/src/app/app-routing.module.ts
+++ burger-queen/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+import { OrdersComponent } from './views/orders/orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'login', component: LoginComponent},
+  {path: 'orders', component: OrdersComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
